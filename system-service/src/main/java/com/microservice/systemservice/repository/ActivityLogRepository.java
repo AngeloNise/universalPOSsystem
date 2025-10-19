a95c09e2db18b2021640ad2480233b518d87aeba
(Int) Angelo <99545791+AngeloNise@users.noreply.github.com>
2025-10-19 13:32:39 +0800

    Delete system-service directory

--- system-service/src/main/java/com/microservice/systemservice/repository/ActivityLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.systemservice.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.microservice.systemservice.models.ActivityLog;
-
-public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
-}
-
