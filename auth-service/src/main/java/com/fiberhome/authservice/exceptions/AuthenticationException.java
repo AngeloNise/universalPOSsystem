9f07801684108bfdb6c97818c69b6aca6a00cd15
(Int) Angelo <99545791+AngeloNise@users.noreply.github.com>
2025-10-19 13:29:13 +0800

    Delete auth-service directory

--- auth-service/src/main/java/com/fiberhome/authservice/exceptions/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package com.fiberhome.authservice.exceptions;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
--- auth-service/src/main/java/com/fiberhome/authservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.fiberhome.authservice.service;
-
-import com.fiberhome.authservice.model.Account;
-
-import java.util.Optional;
-
-public interface UserService {
-   Optional<Account> getByUsername(String username);
-}
